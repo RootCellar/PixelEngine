f3fd55eac3cbf4962f56caef91d9a993cb7d0fb3
RootCellar <RootCellar9877@gmail.com>
2018-11-19 09:26:14 -0900

    Update MouseHelper.java

--- MouseHelper.java
+++ MouseHelper.java
@@ -2,5 +2,40 @@ import PixelEngine.Screen.*;
 
 public class MouseHelper
 {
-
+  PixelCanvas canvas;
+  
+  int dx = 0;
+  int dy = 0;
+  
+  double zoom = 1;
+  
+  public MouseHelper(PixelCanvas c) {
+     canvas = c;
+  }
+  
+  public void setCenter(int x, int y) {
+    dx = x;
+    dy = y;
+  }
+  
+  public void setZoom(double z) {
+    zoom = z; 
+  }
+  
+  public Point getPoint() {
+    Point mousePos = canvas.getMousePosition();
+    
+    mousePos.translate(dx, dy);
+    
+    /*
+    
+    //May or may not work properly
+    mousePos.setLocation( mousePos.getX() * zoom, mousePos.getY() * zoom );
+    
+    */
+    
+    return mousePos;
+    
+  }
+  
 }
