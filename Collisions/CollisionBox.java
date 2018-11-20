29700c60d4d757ffe2774a2d6769b29ed403d330
RootCellar <RootCellar9877@gmail.com>
2018-11-20 09:14:30 -0900

    Create CollisionBox.java

---
+++ Collisions/CollisionBox.java
@@ -0,0 +1,23 @@
+import java.awt.*;
+import java.util.ArrayList;
+
+public class CollisionBox
+{
+  ArrayList<Point> collisionPoints = new ArrayList<Point>();
+  
+  public double top = 0;
+  publc double bottom = 0;
+  public double farLeft = 0;
+  public double farRight = 0;
+  
+  public void addPoint(Point p) {
+    collisionPoints.add(p);
+    
+    if(p.getX() < farLeft) farLeft = p.getX();
+    if(p.getX() > farRight) farRight = p.getX();
+    if(p.getY() < top) top = p.getY();
+    if(p.getY() > bottom) bottom = p.getY();
+    
+  }
+  
+}
