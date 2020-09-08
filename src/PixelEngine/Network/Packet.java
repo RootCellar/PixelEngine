fad3b2eb0f7a60cedad4f59fba15c8e73bbfa25d
RootCellar <rootcellar9877@gmail.com>
2020-09-08 13:02:15 -0800

    Add Comment

--- src/PixelEngine/Network/Packet.java
+++ src/PixelEngine/Network/Packet.java
@@ -1,3 +1,10 @@
+/*
+ *
+ * RootCellar - This class was intended to help handle packets faster and easier,
+ * but it is currently unused. This may be removed in the future.
+ *
+*/
+
 package PixelEngine.Network;
 
 import PixelEngine.Game.*;
