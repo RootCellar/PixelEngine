f9df47a1953d15abc5e11275a3e2fc1239328640
RootCellar <rootcellar9877@gmail.com>
2020-09-08 11:25:10 -0800

    Add ErrorHelper.java

---
+++ src/PixelEngine/Util/ErrorHelper.java
@@ -0,0 +1,20 @@
+/*
+ * RootCellar 9/8/2020
+ * This class will be used to assist with errors and error handling. (This includes exceptions)
+ * This includes converting the error to a string, and helping to provide info associated with the error as well
+*/
+
+
+pubic class ErrorHelper
+{
+	public ErrorHelper() {
+		
+	}
+
+	public String convertExceptionToString(Exception e) {
+		
+	}
+
+
+
+}
